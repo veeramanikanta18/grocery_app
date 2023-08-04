956d31d966a17a34cd50539377a3bfcfbe6815c3
Guntu veera manikanta <manikantadev2@gmail.com>
2023-08-04 23:25:15 +0530

    update code

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 import products_dao
+import uom_dao
 from sql_connection import get_sql_connection
 
 app = Flask(__name__)
@@ -13,6 +14,24 @@ def get_products():
     response = jsonify(products)
     response.headers.add('Access-control-allow-origin','*')
     return response
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/deleteProduct', method=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
 
 if __name__ == "__main__":
     print("starting python flask")
