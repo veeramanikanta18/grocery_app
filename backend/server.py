785c7afc2ceefe667558cc53802af5e96a341ba1
Guntu veera manikanta <manikantadev2@gmail.com>
2023-08-09 21:24:09 +0530

    update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-
 import products_dao
 import uom_dao
 from sql_connection import get_sql_connection
@@ -15,6 +14,7 @@ def get_products():
     response.headers.add('Access-control-allow-origin','*')
     return response
 
+
 @app.route('/getUOM', methods=['GET'])
 def get_uom():
     response = uom_dao.get_uoms(connection)
@@ -22,7 +22,7 @@ def get_uom():
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
-@app.route('/deleteProduct', method=['POST'])
+@app.route('/deleteProduct', methods=['POST'])
 def delete_product():
     return_id = products_dao.delete_product(connection, request.form['product_id'])
     response = jsonify({
@@ -31,6 +31,15 @@ def delete_product():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+@app.route('/insertProduct', methods =['POST'])
+def insert_product():
+    request_payload = request.form['data']
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id' : product_id
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == "__main__":
